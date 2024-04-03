cff15bb7f4fada7a10bbd85e8fc091026961cacd
mdicarlo <marc.dicarlo@harlandclarke.com>
2024-04-03 16:36:21 -0600

    add mason file and extras plugins

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "shellcheck",
+        "pyright",
+        "ansible-language-server ansiblels",
+        "ansible-lint",
+        "black",
+        "flake8",
+        "jedi-language-server",
+        "json-lsp",
+        "lua-language-server",
+        "markdownlint",
+        "marksman",
+        "prettier",
+        "ruff-lsp",
+        "shfmt",
+        "stylua",
+        "tailwindcss-language-server",
+        "terraform-ls",
+        "typescript-language-server",
+        "yaml-language-server",
+      },
+    },
+  },
+}
